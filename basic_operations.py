69f49fd89cab90e5c0d25c5a5cc6ba98578edffe
princeofori <princeofori1470@gmail.com>
2025-06-01 21:16:39 +0000

    initial commit

---
+++ basic_operations.py
@@ -0,0 +1,15 @@
+# basic_operations.py
+
+# Define the two numbers
+number1 = 10
+number2 = 5
+
+# Perform arithmetic operations
+sum_result = number1 + number2
+difference = number1 - number2
+product = number1 * number2
+
+# Print the results
+print("Addition of", number1, "and", number2, "is", sum_result)
+print("Subtraction of", number1, "and", number2, "is", difference)
+print("Multiplication of", number1, "and", number2, "is", product)
