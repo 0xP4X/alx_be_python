8e85b21adb95b9180d34173075a4a97229956d76
princeofori <princeofori1470@gmail.com>
2025-06-16 23:46:06 +0000

    temp_conversion_tool.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -3,6 +3,8 @@
 class BankAccount:
     def __init__(self, initial_balance=0):
         self.__account_balance = initial_balance  # Encapsulated with double underscore
+    def display_balance(self):
+        print(f"Current Balance: ${self.__account_balance:.2f}")
 
     def deposit(self, amount):
         self.__account_balance += amount
